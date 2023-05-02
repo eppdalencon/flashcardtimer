4ef4c5958b44f8215752497bd2455e4fd0545d65
eppdalencon <eppdalencon@gmail.com>
2023-05-02 17:06:08 -0300

    Initial Commit

---
+++ FlashcardTimer/FlashcardTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardTimerApp.swift
+//  FlashcardTimer
+//
+//  Created by Eduardo Dalencon on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
