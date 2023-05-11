d617860ebf4a12ddfae00e546d20c73b6767f978
Leandro Silva <leandro.nobrega2001@gmail.com>
2023-05-11 15:50:19 -0300

    fix toggle

--- FlashcardTimer/DeckConfigView.swift
+++ FlashcardTimer/DeckConfigView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct DeckConfigView: View {
     var deck: Deck
     @State private var showNotification = false
-    @State private var optionsNotification: [Bool] = [false, false]
-    let options = ["Vibrar", "Tocar"]
+    
+    @State var vibrar: Notification = Notification(tipo: "Vibrar", isOn: true)
+    @State var tocar: Notification = Notification(tipo: "Tocar", isOn: true)
     
     var body: some View {
         NavigationStack {
@@ -22,20 +23,30 @@ struct DeckConfigView: View {
                         .font(.title3)
                 }
                 
-                if !showNotification {
+                if showNotification == true {
+                    Toggle(isOn: $vibrar.isOn) {
+                        Text(vibrar.tipo)
+                    }
+                    
+                    Toggle(isOn: $tocar.isOn) {
+                        Text(tocar.tipo)
+                    }
+                    
+                }else {
                     Text("Activate your notifications by clicking the button above")
                         .font(.callout)
                     .foregroundColor(.gray)
-                }else {
-                    ForEach(0 ..< options.count) { index in
-                        Toggle(isOn: $optionsNotification[index]) {
-                            Text(options[index])
-                        }
-                    }
+                    
                 }
             }
             .navigationTitle("Notifications Center")
             .navigationBarTitleDisplayMode(.inline)
+            .onChange(of: vibrar.isOn) { newValue in
+                showNotification = newValue || tocar.isOn
+            }
+            .onChange(of: tocar.isOn) { newValue in
+                showNotification = newValue || vibrar.isOn
+            }
         }
     }
 }
